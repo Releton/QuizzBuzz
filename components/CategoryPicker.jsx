e395026c765efa4e181a68610c410e730f7b86a3
Releton <rinkurick1a@gmail.com>
2023-04-25 23:10:01 +0530

    Make dummy profile pic and dummy progress bar

--- components/CategoryPicker.jsx
+++ components/CategoryPicker.jsx
@@ -9,7 +9,11 @@ function CategoryPicker() {
   };
   return (
     <div className="flex-col relative md:fixed z-50 w-[90%] items-center flex md:w-[30%] xl:w-[20%] mx-auto bg-white bg-opacity-20 backdrop-filter backdrop-blur-sm shadow-lg h-[80vh] rounded-lg mt-6">
-      <div className="h-[30%]"></div>
+      <div className="h-[30%] pt-3 flex flex-col justify-between pb-10 w-full items-center px-3">
+        {/* TODO: Replace with real things */}
+        <div className="w-20 aspect-square rounded-full bg-red-300 text-sm flex items-center justify-center">Profile pic</div>
+        <div className="w-full h-1 bg-green-500 rounded-lg"></div>
+      </div>
       <div className="w-full h-[70%] space-y-5 px-3">
         <div
           onClick={() => setCategory("technology")}
