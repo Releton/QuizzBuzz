2b8ca0bacf464b5c8c9631a10c75feba1da9cd18
Releton <rinkurick1a@gmail.com>
2023-04-29 19:46:09 +0530

    Make the question appear

--- app/page.js
+++ app/page.js
@@ -1,11 +1,51 @@
 import CategoryPicker from "@/components/CategoryPicker";
-
+import { dummy } from "@/data/data";
+import { AiFillLike, AiFillDislike, AiFillEye } from "react-icons/ai";
 export default function Home() {
   return (
-    <main className="px-3">
+    <main className="px-3 flex gap-3 flex-col md:flex-row h-full">
       <CategoryPicker />
-      <div>
+      <div className="h-[80vh] md:w-[50%] w-[90%] overflow-y-auto relative flex flex-col gap-3 p-3 mx-6 z-40 border-2 px-3 border-white rounded-lg backdrop-filter backdrop-blur-md bg-white bg-opacity-10 mt-6">
+        {dummy.ok &&
+          dummy.data.map((data, i) => {
+            return (
+              <div
+                key={i}
+                className="border-2 shadow-lg border-white justify-between flex rounded-lg p-3 bg-gradient-to-r from-white to-gray-300 bg-opacity-75 text-slate-900"
+              >
+                <div className="space-y-4 max-w-[85%] min-w-[85%]">
+                  <p className="overflow-hidden lg:text-xl text-base font-bold  tracking-widest">
+                    {data.heading}
+                  </p>
+                  <p className="text-gray-500">{data.description}</p>
+                  <div className="flex justify-between">
+                    <div className="bg-gradient-to-r inline-block p-1 rounded-lg text-white from-blue-500 to-red-500">
+                      {data.category}
+                    </div>
+                    <div className="text-red-500 font-extrabold text-lg items-center">
+                      {data.totalMarks + " marks"}
+                    </div>
+                    <div className="items-center gap-1 flex">
+                      <AiFillLike />
+                      <p>{data.likes}</p>
+                    </div>
+                    <div className="items-center gap-1 flex">
+                      <AiFillEye />
+                      <p>{data.views}</p>
+                    </div>
+                    <div className="items-center gap-1 flex">
+                      <p className="tracking-tighter text-sm font-extralight">{data.author}</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-col justify-evenly">
+                  <AiFillLike fontSize={25} className="text-slate-900 cursor-pointer hover:text-green-500 duration-200" />
+                  <AiFillDislike fontSize={25} className="text-slate-900 cursor-pointer hover:text-red-500 duration-200" />
+                </div>
+              </div>
+            );
+          })}
       </div>
     </main>
-  )
+  );
 }
--- components/CategoryPicker.jsx
+++ components/CategoryPicker.jsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState } from "react";
+import CategoryItem from "./sub/categoryItem";
+import { categories } from "@/data/data";
 
 function CategoryPicker() {
   const [selectedCategory, setSelectedCategory] = useState();
@@ -8,37 +10,28 @@ function CategoryPicker() {
     setSelectedCategory(cat);
   };
   return (
-    <div className="flex-col relative md:fixed z-50 w-[90%] items-center flex md:w-[30%] xl:w-[20%] mx-auto bg-white bg-opacity-20 backdrop-filter backdrop-blur-sm shadow-lg h-[80vh] rounded-lg mt-6">
+    <div className="flex-col relative  z-40 w-[90%] items-center flex md:w-[30%] xl:w-[20%] mx-auto md:mx-0 bg-white bg-opacity-20 backdrop-filter backdrop-blur-sm shadow-lg h-[80vh] rounded-lg mt-6">
       <div className="h-[30%] pt-3 flex flex-col justify-between pb-10 w-full items-center px-3">
         {/* TODO: Replace with real things */}
-        <div className="w-20 aspect-square rounded-full bg-red-300 text-sm flex items-center justify-center">Profile pic</div>
+        <div className="w-20 aspect-square rounded-full bg-red-300 text-sm flex items-center justify-center">
+          Profile pic
+        </div>
         <div className="w-full h-1 bg-green-500 rounded-lg"></div>
       </div>
-      <div className="w-full h-[70%] space-y-5 px-3">
-        <div
-          onClick={() => setCategory("technology")}
-          className={`bg-white bg-opacity-20 border-2 border-white w-full h-[20%] rounded-lg shadow-md flex justify-center items-center text-white cursor-pointer hover:scale-105 duration-150 delay-100 ${selectedCategory=="technology" ? 'bg-blue-500 font-bold bg-opacity-80' : ''}`}
-        >
-          Technology
-        </div>
-        <div
-          onClick={() => setCategory("science")}
-          className={`bg-white bg-opacity-20 border-2 border-white w-full h-[20%] rounded-lg shadow-md flex justify-center items-center text-white cursor-pointer hover:scale-105 duration-150 delay-100 ${selectedCategory=="science" ? 'bg-blue-500 font-bold bg-opacity-80' : ''}`}
-        >
-          Science
-        </div>
-        <div
-          onClick={() => setCategory("literature")}
-          className={`bg-white bg-opacity-20 border-2 border-white w-full h-[20%] rounded-lg shadow-md flex justify-center items-center text-white cursor-pointer hover:scale-105 duration-150 delay-100 ${selectedCategory=="literature" ? 'bg-blue-500 font-bold bg-opacity-80' : ''}`}
-        >
-          Literature
-        </div>
-        <div
-          onClick={() => setCategory("others")}
-          className={`bg-white bg-opacity-20 border-2 border-white w-full h-[20%] rounded-lg shadow-md flex justify-center items-center text-white cursor-pointer hover:scale-105 duration-150 delay-100 ${selectedCategory=="others" ? 'bg-blue-500 font-bold bg-opacity-80' : ''}`}
-        >
-          Others
-        </div>
+      <div className="w-full h-[70%] flex flex-col gap-2 pb-5 px-3">
+        {Object.keys(categories).map((cat) => {
+          const category = categories[cat];
+          return (
+            <CategoryItem
+              value={category.value}
+              url={category.url}
+              setCategory={setCategory}
+              selectedCategory={selectedCategory}
+            >
+              {category?.text}
+            </CategoryItem>
+          );
+        })}
       </div>
     </div>
   );
